32ecd0bec6ca92b3c5dd10f0bf52ce02d87b9ae2
Josip <josip.sadek@racunarstvo.hr>
2021-10-31 00:15:57 +0200

    Initial commit

---
+++ src/hr/algebra/mainWindow/controller/MainWindowController.java
@@ -0,0 +1,14 @@
+package hr.algebra.mainWindow.controller;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainWindowController implements Initializable {
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
